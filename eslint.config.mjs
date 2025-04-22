8b7754b5659fde8a7b72bded251bb5a36d553477
janet93techgirl <abenarhdavidson@gmail.com>
2025-04-22 10:52:47 -0500

    final update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,42 @@
+import babelEslintParser from "@babel/eslint-parser";
+
 export default [
   {
     ignores: ["node_modules", "dist"],
   },
   {
-    files: [".js", ".jsx", ".ts", ".tsx"],
+    files: ["*.js", "*.jsx"],
+    plugins: ["react", "react-hooks", "import", "next"],
+    extends: [
+      "next",
+      "next/core-web-vitals",
+      "plugin:react-hooks/recommended",
+      "plugin:import/errors",
+      "plugin:import/warnings"
+    ],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      parser: "@babel/eslint-parser",
+      parserOptions: {
+        requireConfigFile: false,
+        babelOptions: {
+          presets: ["next/babel"],
+        },
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    rules: {
+      "react/jsx-uses-react": "error",
+      "react/jsx-uses-vars": "error",
+      "no-unused-vars": "warn",
+      "no-console": "off",
+    },
+  },
+  {
+    files: ["*.ts", "*.tsx"],
     plugins: ["react", "react-hooks", "@typescript-eslint", "import", "next"],
     extends: [
       "next",
